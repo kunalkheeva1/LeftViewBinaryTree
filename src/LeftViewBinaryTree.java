8e0e035b0ae89f8ec19383b3be91614dd31ef2fe
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-17 22:19:49 -0700

    created a tree class

--- src/LeftViewBinaryTree.java
+++ src/LeftViewBinaryTree.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
+
 public class LeftViewBinaryTree {
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer>
     public static void main(String[] args) {
-        
+
     }
 }
