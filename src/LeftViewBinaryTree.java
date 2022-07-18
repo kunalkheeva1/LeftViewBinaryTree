79d42264215a2d4dfc40fa04c9c3209301f7bc18
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-17 22:26:13 -0700

    added comments for future references

--- src/LeftViewBinaryTree.java
+++ src/LeftViewBinaryTree.java
@@ -15,24 +15,29 @@ public class LeftViewBinaryTree {
     }
 
     public List<Integer> leftViewBinaryTree(TreeNode root) {
+        //get a list to be printd
         ArrayList<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-
+        //enter the levels one by one in the queue
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-
+        //until the queue contains nothing
         while(!q.isEmpty()){
             int size = q.size();
+            // but for the initial level
             for(int i= 0; i<size; i++){
+                // get a root pointer
                 TreeNode current = root;
+                //add the value of 0th index element which is first from the left
                 if(i ==0){
                     ans.add(current.val);
                 }
+                // and check the left and right of the current
                 if(current.left != null) q.add(current.left);
                 if(current.right  != null) q.add(current.right);
             }
 
-        }return ans
+        }return ans;
 
     }
     public static void main(String[] args) {
